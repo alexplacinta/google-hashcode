c8ec084e74d09bf16934d164d0e13d3027e605cd
Alex <alexplacinta94@gmail.com>
2020-02-12 00:49:02 +0200

    das

--- 2019-qual.cpp
+++ 2019-qual.cpp
@@ -2,57 +2,58 @@
 
 using namespace std;
 
-#define BIT_SET_SIZE 220
+#define BIT_SET_SIZE 500
 
 class Hashcode {
 public:
     struct Photo {
         char type;
         int n_tags;
-        unordered_set<int> tags;
-//        bitset<BIT_SET_SIZE> tags;
+//        vector<int> tags;
+        bitset<BIT_SET_SIZE> tags;
     };
     struct Slide {
         char type;
         int firstPhoto;
         int secondPhoto;
-        unordered_set<int> tags;
-//        bitset<BIT_SET_SIZE> tags;
+//        vector<int> tags;
+        bitset<BIT_SET_SIZE> tags;
     };
 
     void solve(ifstream &in, ofstream &out) {
         int n_photos;
         in >> n_photos;
         vector<Photo> photos(n_photos);
-        int hCount = 0;
-        int vCount = 0;
         unordered_map<string, int> tagToId;
+        unordered_map<int, unordered_map<char, unordered_set<int>>> tagIdToPhotos;
         int tagCounter = 0;
         int totalTags = 0;
         for (int i = 0; i < n_photos; ++i) {
             in >> photos[i].type >> photos[i].n_tags;
-            if (photos[i].type == 'H') {
-                hCount++;
-            } else {
-                vCount++;
-            }
             for (int j = 0; j < photos[i].n_tags; ++j) {
                 string tag;
                 in >> tag;
                 if (tagToId.find(tag) == tagToId.end()) {
                     tagCounter++;
-                    tagToId.insert(make_pair(tag, tagCounter));
+                    tagToId.insert({tag, tagCounter});
+                    tagToId[tag] = tagCounter;
+                    tagIdToPhotos[tagCounter] = {
+                            {'H', unordered_set<int>()},
+                            {'V', unordered_set<int>()}
+                    };
                 }
-                photos[i].tags.insert(tagToId.find(tag)->second);
-//                photos[i].tags.set(tagToId.find(tag)->second - 1, true);
+//                photos[i].tags.push_back(tagToId[tag]);
+                photos[i].tags.set(tagToId[tag] - 1, true);
+                tagIdToPhotos[tagToId[tag]][photos[i].type].insert(i);
             }
+//            sort(photos[i].tags.begin(), photos[i].tags.end());
             totalTags += photos[i].n_tags;
         }
         cout << tagCounter << "-" << totalTags << endl;
 
         vector<Slide> slides;
-        unordered_set<int> hPhotos;
-        unordered_set<int> vPhotos;
+        set<int> hPhotos;
+        set<int> vPhotos;
         for (int i = 0; i < n_photos; ++i) {
             if (photos[i].type == 'H') {
                 hPhotos.insert(i);
@@ -61,57 +62,92 @@ public:
             }
         }
 
-        auto unionVPhotoTags = [&](unordered_set<int> &first, unordered_set<int> &second) {
-            unordered_set<int> result;
-            for (const auto &i : first) {
-                result.insert(i);
-            }
-            for (const auto &i : second) {
-                result.insert(i);
+//        auto unionVPhotoTags = [&](vector<int> &first, vector<int> &second) {
+//            set<int> result;
+//            for (const auto &i : first) {
+//                result.insert(i);
+//            }
+//            for (const auto &i : second) {
+//                result.insert(i);
+//            }
+//            return vector<int>(result.begin(), result.end());
+//        };
+        auto unionVPhotoTags = [&](bitset<BIT_SET_SIZE> &first, bitset<BIT_SET_SIZE> &second) {
+            return first | second;
+        };
+
+//        auto score = [&](vector<int> &a, vector<int> &b) {
+//            int intersectCount = 0;
+//            int sizeA = a.size();
+//            int sizeB = b.size();
+//            int iA = 0;
+//            int iB = 0;
+//            while (iA < sizeA && iB < sizeB) {
+//                if (a[iA] == b[iB]) {
+//                    intersectCount++;
+//                    iA++;
+//                    iB++;
+//                } else if (a[iA] > b[iB]) {
+//                    iB++;
+//                } else {
+//                    iA++;
+//                }
+//            }
+//
+//            return min(min(sizeA - intersectCount, intersectCount), sizeB - intersectCount);
+//        };
+        auto score = [&](bitset<BIT_SET_SIZE> &a, bitset<BIT_SET_SIZE> &b) {
+            int intersectCount = (int)(a & b).count();
+            return min({(int)a.count() - intersectCount, intersectCount, (int)b.count() - intersectCount});
+        };
+
+//        auto getTags = [&](vector<int>& tags) {
+//            return tags;
+//        };
+        auto getTags = [&](bitset<BIT_SET_SIZE>& tags) {
+            vector<int> result;
+            for (int i = 0; i < tags.size(); ++i) {
+                if (tags[i]) {
+                    result.push_back(i+1);
+                }
             }
             return result;
         };
-//        auto unionVPhotoTags = [&](bitset<BIT_SET_SIZE> &first, bitset<BIT_SET_SIZE> &second) {
-//            return first | second;
+
+//        auto getPosiblePhotos = [&](Slide &slide, char type) {
+//            unordered_set<int> posiblePhotos;
+//            for (auto& tag : getTags(slide.tags)) {
+//                for (auto &photoId : tagIdToPhotos[tag][type]) {
+//                    posiblePhotos.insert(photoId);
+//                }
+//            }
+//            return posiblePhotos;
 //        };
 
-        auto score = [&](unordered_set<int> &a, unordered_set<int> &b) {
-            int intersectCount = 0;
-            int aSize = a.size();
-            int bSize = b.size();
-            if (aSize <= bSize) {
-                for (const int &i : a) {
-                    if (b.find(i) != b.end()) {
-                        intersectCount++;
-                    }
-                }
+        auto erasePhoto = [&](int& id) {
+            if (photos[id].type == 'H') {
+                hPhotos.erase(id);
             } else {
-                for (const int &i : b) {
-                    if (a.find(i) != a.end()) {
-                        intersectCount++;
-                    }
-                }
+                vPhotos.erase(id);
             }
 
-            return min(min(aSize - intersectCount, intersectCount), bSize - intersectCount);
+            for (auto tag : getTags(photos[id].tags)) {
+                tagIdToPhotos[tag][photos[id].type].erase(id);
+            }
         };
-//        auto score = [&](bitset<BIT_SET_SIZE> &a, bitset<BIT_SET_SIZE> &b) {
-//            int intersectCount = (int)(a & b).count();
-//            return min({(int)a.count() - intersectCount, intersectCount, (int)b.count() - intersectCount});
-//        };
 
         Slide firstSlide;
-        if (hCount > 0) {
+        if (hPhotos.size() > 0) {
             int hPhoto = *hPhotos.begin();
-            hPhotos.erase(hPhotos.begin());
+            erasePhoto(hPhoto);
             firstSlide.type = photos[hPhoto].type;
             firstSlide.firstPhoto = hPhoto;
             firstSlide.tags = photos[hPhoto].tags;
         } else {
             int firstVPhoto = *vPhotos.begin();
-            vPhotos.erase(vPhotos.begin());
+            erasePhoto(firstVPhoto);
             int secondVPhoto = *vPhotos.begin();
-            vPhotos.erase(vPhotos.begin());
+            erasePhoto(secondVPhoto);
             firstSlide.type = photos[firstVPhoto].type;
             firstSlide.firstPhoto = firstVPhoto;
             firstSlide.secondPhoto = secondVPhoto;
@@ -123,43 +159,62 @@ public:
             Slide &lastSlide = slides.back();
             int best1stP = 0;
             int bestScore = -1;
-            for (int i : hPhotos) {
-                int tempScore = score(lastSlide.tags, photos[i].tags);
-                if (tempScore > bestScore) {
-                    bestScore = tempScore;
-                    best1stP = i;
+//            auto posibleHPhotos = getPosiblePhotos(lastSlide, 'H');
+//            auto posibleVPhotos = getPosiblePhotos(lastSlide, 'V');
+            auto& posibleHPhotos = hPhotos;
+            auto& posibleVPhotos = vPhotos;
+            if (posibleHPhotos.size() > 0 || posibleVPhotos.size() > 0) {
+                for (int i : posibleHPhotos) {
+                    int tempScore = score(lastSlide.tags, photos[i].tags);
+                    if (tempScore > bestScore) {
+                        bestScore = tempScore;
+                        best1stP = i;
+                    }
+                }
+                for (int i : posibleVPhotos) {
+                    int tempScore = score(lastSlide.tags, photos[i].tags);
+                    if (tempScore > bestScore) {
+                        bestScore = tempScore;
+                        best1stP = i;
+                    }
+                }
+            } else {
+                if (hPhotos.size() > 0) {
+                    best1stP = *hPhotos.begin();
+                } else {
+                    best1stP = *vPhotos.begin();
                 }
             }
-            for (int i : vPhotos) {
-                int tempScore = score(lastSlide.tags, photos[i].tags);
-                if (tempScore > bestScore) {
-                    bestScore = tempScore;
-                    best1stP = i;
-                }
-            }
+
+            erasePhoto(best1stP);
             Slide nextSlide;
             if (photos[best1stP].type == 'H') {
-                hPhotos.erase(best1stP);
                 nextSlide.type = photos[best1stP].type;
                 nextSlide.firstPhoto = best1stP;
                 nextSlide.tags = photos[best1stP].tags;
                 slides.push_back(nextSlide);
             } else {
-                vPhotos.erase(best1stP);
                 if (!vPhotos.empty()) {
                     int best2ndP = 0;
                     int bestScoreV = -1;
 
-                    for (int i : vPhotos) {
-                        auto mergedTags = unionVPhotoTags(photos[best1stP].tags, photos[i].tags);
-                        int tempScore = score(lastSlide.tags, mergedTags);
-                        if (tempScore > bestScoreV) {
-                            bestScoreV = tempScore;
-                            best2ndP = i;
+//                    auto posibleVPhotos2 = getPosiblePhotos(lastSlide, 'V');
+                    auto& posibleVPhotos2 = vPhotos;
+                    if (!posibleVPhotos2.empty()) {
+                        for (int i : posibleVPhotos2) {
+                            auto mergedTags = unionVPhotoTags(photos[best1stP].tags, photos[i].tags);
+                            int tempScore = score(lastSlide.tags, mergedTags);
+                            if (tempScore > bestScoreV) {
+                                bestScoreV = tempScore;
+                                best2ndP = i;
+                            }
                         }
+                    } else {
+                        best2ndP = *vPhotos.begin();
                     }
-                    vPhotos.erase(best2ndP);
 
+
+                    erasePhoto(best2ndP);
                     nextSlide.type = photos[best1stP].type;
                     nextSlide.firstPhoto = best1stP;
                     nextSlide.secondPhoto = best2ndP;
@@ -178,7 +233,7 @@ public:
         for (int k = 0; k < slides.size() - 1; ++k) {
             totalScore += score(slides[k].tags, slides[k + 1].tags);
         }
-        cout << totalScore << endl;
+        cerr << totalScore << endl;
 //        for (int k = 0; k < slides.size(); ++k) {
 //            cout << slides[k].firstPhoto << slides[k].type << endl;
 //        }
@@ -187,12 +242,14 @@ public:
 };
 
 int main() {
-    string fileDirectory = "/home/alex/workspace/task/files/";
-    string filename = "b_lovely_landscapes.txt";
+    string fileDirectory = "/home/alex/Workspace/google-hashcode/files/";
+    string filename = "e_shiny_selfies.txt";
 
     Hashcode hashcode;
     ifstream in(fileDirectory + "in/" + filename);
     ofstream out(fileDirectory + "out/" + filename);
+    const clock_t begin_time = clock();
     hashcode.solve(in, out);
+    std::cout << float(clock() - begin_time) / CLOCKS_PER_SEC << endl;
     return 0;
 }
